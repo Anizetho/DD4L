99c4d6a7ee85aeb511df6570d4f7f500f95eaf2d
Thomas Anizet <AnizetThomas@mbp-de-thomas-2.ecam.be>
2017-11-06 18:08:21 +0100

    06/11/12 18:08

---
+++ src/main/java/DD4L/Calculator.java
@@ -0,0 +1,17 @@
+package DD4L;
+
+
+public class Calculator {
+	
+    public int a;
+    public int b;
+    
+    public int add(int a, int b) {
+            return a + b;
+    }
+
+    public int sub(int a, int b) {
+            return a - b;
+    }
+
+}
---
+++ src/test/java/DD4L/CalculatorTest.java
@@ -0,0 +1,26 @@
+package DD4L;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class CalculatorTest {
+
+	@Test
+	public final void testAdd() {
+		Calculator calc = new Calculator();
+		int expResult = 5;
+		int result = calc.add(3, 2);
+	    assertEquals(expResult, result);
+	    }
+	@Test
+	public final void testSub() {
+		Calculator calc = new Calculator();
+		int expResult = 1;
+		int result = calc.sub(3, 2);
+	    assertEquals(expResult, result);
+	    }
+}
+
+
+
